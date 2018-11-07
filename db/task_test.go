26974e0e17cd1d56b44e873a0d834377caae2827
Adrian L Lange <mail@p3lim.net>
2018-11-07 16:02:47 +0100

    Implement db task tests

--- db/task_test.go
+++ db/task_test.go
@@ -1,18 +1,51 @@
 package db
 
-import "testing"
+import (
+	"testing"
 
-func TestFindByID(t *testing.T) {
+	"hacking-portal/models"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTaskUpsert(t *testing.T) {
+	// new database type
 	tdb := TaskDatabase{}
-	// TODO
+
+	// attempt to insert
+	task, err := tdb.Upsert(models.Task{
+		Description: "foo",
+		Answer:      "bar",
+	})
+
+	// assert output
+	require.Nil(t, err, "failed to insert task")
+	require.EqualValues(t, 1, task.ID)
+	require.EqualValues(t, "foo", task.Description)
+	require.EqualValues(t, "bar", task.Answer)
 }
 
-func TestFindTasks(t *testing.T) {
+func TestTaskFindAll(t *testing.T) {
+	// new database type
 	tdb := TaskDatabase{}
-	// TODO
+
+	// attempt to find tasks
+	tasks, err := tdb.FindAll()
+
+	// assert output
+	require.Nil(t, err, "failed to get tasks")
+	require.Len(t, tasks, 1) // this runs after upsert, so there should be 1
 }
 
-func TestUpsert(t *testing.T) {
+func TestTaskFindByID(t *testing.T) {
+	// new database type
 	tdb := TaskDatabase{}
-	// TODO
+
+	// attempt to find tasks
+	task, err := tdb.FindByID(1)
+
+	// assert output
+	require.Nil(t, err, "failed to get task")
+	require.EqualValues(t, "foo", task.Description)
+	require.EqualValues(t, "bar", task.Answer)
 }
