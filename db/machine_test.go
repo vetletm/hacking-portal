60a4bb6003cdd1a00b5c275e40e7ec97ab0375a6
Adrian L Lange <mail@p3lim.net>
2018-11-07 15:03:58 +0100

    Implement db machine tests

--- db/machine_test.go
+++ db/machine_test.go
@@ -1,23 +1,56 @@
 package db
 
-import "testing"
+import (
+	"testing"
 
-func TestFindAll(t *testing.T) {
+	"hacking-portal/models"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMachineUpsert(t *testing.T) {
+	// new database type
 	tdb := MachineDatabase{}
-	// TODO
+
+	// attempt to insert
+	err := tdb.Upsert(models.Machine{"1234", "foo", 1})
+
+	// assert output
+	require.Nil(t, err, "failed to insert machine")
 }
 
-func TestFindByID(t *testing.T) {
+func TestMachineFindAll(t *testing.T) {
+	// new database type
 	tdb := MachineDatabase{}
-	// TODO
+
+	// attempt to find all machines
+	machines, err := tdb.FindAll()
+
+	// assert output
+	require.Nil(t, err, "failed to get machines")
+	require.Len(t, machines, 1) // this runs after upsert, so there should be 1
 }
 
-func TestFindByGroup(t *testing.T) {
+func TestMachineFindByID(t *testing.T) {
+	// new database type
 	tdb := MachineDatabase{}
-	// TODO
+
+	// attempt to find machine by ID
+	machine, err := tdb.FindByID("1234") // from the Upsert test
+
+	// assert output
+	require.Nil(t, err, "failed to get single machine")
+	require.EqualValues(t, "foo", machine.Name)
 }
 
-func TestUpsert(t *testing.T) {
+func TestMachineFindByGroup(t *testing.T) {
+	// new database type
 	tdb := MachineDatabase{}
-	// TODO
+
+	// attempt to find machines by group
+	machines, err := tdb.FindByGroup(1) // from the Upsert test
+
+	// assert output
+	require.Nil(t, err, "failed to get machines")
+	require.Len(t, machines, 1) // this runs after upsert, so there should be 1
 }
