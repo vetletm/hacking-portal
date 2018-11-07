481aedf77526512437734b880b2bb12a0d315bde
Adrian L Lange <mail@p3lim.net>
2018-11-07 14:56:50 +0100

    Setup database testing environment with mgo

--- db/db_test.go
+++ db/db_test.go
@@ -1,7 +1,31 @@
 package db
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/globalsign/mgo/dbtest"
+)
 
 func TestMain(m *testing.M) {
-	// TODO
+	// temp directory to store test database
+	tempDir, _ := ioutil.TempDir("", "testing")
+
+	// start the test database server and get a session
+	var server dbtest.DBServer
+	server.SetPath(tempDir)
+	session := server.Session()
+
+	// set the database variable to the database in a session
+	db = session.DB("testing")
+
+	// run the tests
+	ret := m.Run()
+
+	// cleanup
+	db.DropDatabase()
+	session.Close()
+	server.Stop()
+	os.Exit(ret)
 }
