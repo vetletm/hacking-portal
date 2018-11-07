86348adfc4af9203480aea23f5b543f38b797e28
Adrian L Lange <mail@p3lim.net>
2018-11-07 16:02:33 +0100

    Rename task db method

--- db/task.go
+++ db/task.go
@@ -12,26 +12,26 @@ import (
 
 // TaskStorage is an interface describing the methods of the TaskDatabase struct
 type TaskStorage interface {
+	FindAll() ([]models.Task, error)
 	FindByID(int) (models.Task, error)
-	FindTasks() ([]models.Task, error)
 	Upsert(models.Task) (models.Task, error)
 }
 
 // TaskDatabase is an implementation of the storage for all Task-related methods
 type TaskDatabase struct{}
 
+// FindAll returns an array of all the group IDs
+func (TaskDatabase) FindAll() ([]models.Task, error) {
+	var tasks []models.Task
+	err := db.C("tasks").Find(nil).All(&tasks)
+	return tasks, err
+}
+
 // FindByID returns a single task by ID
 func (TaskDatabase) FindByID(id int) (models.Task, error) {
 	var task models.Task
 	err := db.C("tasks").Find(bson.M{"id": id}).One(&task)
 	return task, err
-}
-
-// FindGroups returns an array of all the group IDs
-func (TaskDatabase) FindTasks() ([]models.Task, error) {
-	var tasks []models.Task
-	err := db.C("tasks").Find(nil).All(&tasks)
-	return tasks, err
 }
 
 // Upsert adds/updates the task to the database and returns the updated version
