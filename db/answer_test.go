04e9333b93229d632ae7f0250b0e67093748ae7f
Adrian L Lange <mail@p3lim.net>
2018-11-07 14:57:16 +0100

    Implement db answer tests
    100% coverage

--- db/answer_test.go
+++ db/answer_test.go
@@ -1,23 +1,59 @@
 package db
 
-import "testing"
+import (
+	"testing"
 
-func TestFindAll(t *testing.T) {
-	tdb := TestDatabase{}
-	// TODO
+	"hacking-portal/models"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAnswerUpsert(t *testing.T) {
+	// new database type
+	tdb := AnswerDatabase{}
+
+	// attempt to insert
+	answer, err := tdb.Upsert(models.Answer{1, 1, "foo"})
+
+	// assert output
+	require.Nil(t, err, "failed to insert answer")
+	require.EqualValues(t, 1, answer.TaskID)
+	require.EqualValues(t, 1, answer.GroupID)
+	require.EqualValues(t, "foo", answer.Answer)
 }
 
-func TestFindByID(t *testing.T) {
-	tdb := TestDatabase{}
-	// TODO
+func TestAnswerFindAll(t *testing.T) {
+	// new database type
+	tdb := AnswerDatabase{}
+
+	// attempt to find
+	answers, err := tdb.FindAll()
+
+	// assert output
+	require.Nil(t, err, "failed to get answers")
+	require.Len(t, answers, 1) // this runs after upsert, so there should be 1
 }
 
-func TestFindByGroup(t *testing.T) {
-	tdb := TestDatabase{}
-	// TODO
+func TestAnswerFindByID(t *testing.T) {
+	// new database type
+	tdb := AnswerDatabase{}
+
+	// attempt to find answer by ID
+	answer, err := tdb.FindByID(1) // from the Upsert test
+
+	// assert output
+	require.Nil(t, err, "failed to get single answer")
+	require.EqualValues(t, 1, answer.TaskID)
 }
 
-func TestUpsert(t *testing.T) {
-	tdb := TestDatabase{}
-	// TODO
+func TestAnswerFindByGroup(t *testing.T) {
+	// new database type
+	tdb := AnswerDatabase{}
+
+	// attempt to find answer by group
+	answers, err := tdb.FindByGroup(1) // from the Upsert test
+
+	// assert output
+	require.Nil(t, err, "failed to get single answer")
+	require.Len(t, answers, 1) // this runs after upsert, so there should be 1
 }
