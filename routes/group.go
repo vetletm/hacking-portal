e26dcf0ca2422e75805f968b9027b5103d5eff01
Adrian L Lange <mail@p3lim.net>
2018-11-14 21:30:28 +0100

    Add page for the group view

--- routes/group.go
+++ routes/group.go
@@ -1,9 +1,12 @@
 package routes
 
 import (
+	"html/template"
 	"net/http"
+	"path"
 
 	"hacking-portal/db"
+	"hacking-portal/models"
 
 	"github.com/go-chi/chi"
 )
@@ -15,9 +18,44 @@ type GroupEndpoint struct {
 	Students db.StudentStorage
 }
 
+type groupPageData struct {
+	User     models.Student
+	Machines []models.Machine
+}
+
 // GetDashboard renders a view of the group interface
 func (storage *GroupEndpoint) GetDashboard(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	// get the user from the session (type-casted)
+	username := r.Context().Value("session_user_id").(string)
+
+	// get the actual sessionUser object from the username
+	sessionUser, err := storage.Students.FindByID(username)
+	if err != nil || sessionUser.GroupID == 0 {
+		// sessionUser doesn't exist or has no group affiliation, redirect em'
+		http.Redirect(w, r, "/groups", http.StatusTemporaryRedirect)
+		return
+	}
+
+	// prepare page data
+	pageData := groupPageData{User: sessionUser}
+
+	// get the machines
+	// TODO: get from OpenStack
+	pageData.Machines = []models.Machine{
+		{"123", 0, 1, "10.212.136.10"},
+		{"456", 0, 2, "10.212.136.20"},
+		{"789", 0, 3, "10.212.136.30"},
+	}
+
+	// prepare and ensure validity of template files
+	tpl := template.Must(template.ParseFiles(
+		path.Join("templates", "layout.html"),
+		path.Join("templates", "navigation.html"),
+		path.Join("templates", "group.html"),
+	))
+
+	// render the templates with data
+	tpl.ExecuteTemplate(w, "layout", pageData)
 }
 
 // GetMachineKey returns the given group's machine's PEM key
