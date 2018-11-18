dcd19ef42863671615b47d5ebadcd4937688ff61
vetletm <vetletm@stud.ntnu.no>
2018-11-18 20:48:06 +0100

    Add TestGroupMachineRestart without contacting OpenStack

--- routes/group_test.go
+++ routes/group_test.go
@@ -56,7 +56,55 @@ func TestGroupDashboard(t *testing.T) {
 }
 
 func TestGroupMachineRestart(t *testing.T) {
-	// TODO:
+	mdb := new(mockMachineStorage)
+	mdb.Upsert(models.Machine{
+		Name:    "test1",
+		UUID:    "1111",
+		GroupID: 2,
+		Address: "1.1.1.1",
+	})
+
+	sdb := new(mockStudentStorage)
+	sdb.Upsert(models.Student{"ungrouped", "Ungrouped User", 0})
+	sdb.Upsert(models.Student{"grouped", "Grouped User", 1})
+
+	testData := []struct {
+		uuid string
+		code int
+		user string
+	}{
+		{uuid: "", code: http.StatusBadRequest, user: "invalid"},
+		{uuid: "", code: http.StatusBadRequest, user: "ungrouped"},
+		{uuid: "0000", code: http.StatusBadRequest, user: "grouped"},
+		{uuid: "1111", code: http.StatusBadRequest, user: "grouped"},
+	}
+
+	for _, data := range testData {
+		// create a request to pass to the handler
+		req := httptest.NewRequest("POST", "/", nil)
+		req = req.WithContext(context.WithValue(req.Context(), "session_user_id", data.user))
+
+		// create a response recorder to record the response from the handler
+		res := httptest.NewRecorder()
+
+		// prepare the endpoint with mocked storage
+		ep := GroupEndpoint{
+			Students: sdb,
+			Machines: mdb,
+		}
+
+		// prepare context
+		ctx := chi.NewRouteContext()
+		ctx.URLParams.Add("uuid", data.uuid)
+		req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+		// serve the handler
+		handler := http.HandlerFunc(ep.PostMachineRestart)
+		handler.ServeHTTP(res, req)
+
+		// test the status
+		require.Equal(t, data.code, res.Code, "handler returned wrong status code")
+	}
 }
 
 func TestGetLeaveGroup(t *testing.T) {
